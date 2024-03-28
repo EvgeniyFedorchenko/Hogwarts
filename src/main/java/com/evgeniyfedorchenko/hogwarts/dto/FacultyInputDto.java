f765b4fb369a0245091eade507bd2ff689558a43
EvgeniyFedorchenko <jecky432@gmail.com>
2024-03-29 02:30:25 +0300

    change validation FacultyInputDto

--- src/main/java/com/evgeniyfedorchenko/hogwarts/dto/FacultyInputDto.java
+++ src/main/java/com/evgeniyfedorchenko/hogwarts/dto/FacultyInputDto.java
@@ -3,13 +3,14 @@ package com.evgeniyfedorchenko.hogwarts.dto;
 
 import com.evgeniyfedorchenko.hogwarts.entities.Color;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class FacultyInputDto {
 
     @NotBlank(message = "Faculty name cannot be empty")
     private String name;
 
-    @NotBlank(message = "Faculty color cannot be empty")
+    @NotNull(message = "Faculty color cannot be empty")
     private Color color;
 
     public String getName() {
